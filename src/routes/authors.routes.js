6075b94f163c2b9e9a731728af50162d74c6a021
devSummon99 <michelperezsomoza@gmail.com>
2024-09-01 19:52:28 -0400

    post get put mothods

--- src/routes/authors.routes.js
+++ src/routes/authors.routes.js
@@ -5,19 +5,35 @@ import { PrismaClient } from "@prisma/client";
 const router = Router();
 const prisma = new PrismaClient();
 
-router.get("/authors", async(req, res) => {
+router.get("/authors", async (req, res) => {
   const authors = await prisma.author.findMany();
- 
-    return (authors > 0) ?  res.json(authors) : res.json("Error 404");
+
+  return authors.length > 0
+    ? res.json(authors)
+    : res.json("No existe ningun libro");
 });
+
+router.get("/authors/:id" , async (req, res) => {
+  
+})
 
 router.post("/authors", async (req, res) => {
-  const newAuthor = await prisma.author.create({
-    data: req.body,
-  });
-  return res.json(newAuthor);
-   
+  const author = await prisma.author.findFirst({ where: { name: req.body.name } });
+  if (!author) {
+    const newAuthor = await prisma.author.create({
+      data: req.body,
+    });
+    return res.json(newAuthor);
+  }
+  return res.json("El autor ya existe");
 });
 
+router.put("/authors/:id", async (req, res) => {
+
+})
+
+router.delete("/authors/:id", async (req, res) => {
+
+})
 
 export default router;
--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -6,17 +6,36 @@ const router = Router();
 const prisma = new PrismaClient();
 
 router.get("/books", async (req, res) => {
-  const books = await prisma.book.findMany();
+  const books = await prisma.book.findMany(
+    {include: {author:true}},
+  );
 
-  return (books.length > 0) ? res.json(books): res.json("No se encontraron libros");
+  return books.length > 0
+    ? res.json(books)
+    : res.json("No se encontraron libros");
 });
 
-router.post("/books", async (req,res) => {
- 
-  const newBook = await prisma.book.create({
-    data: req.body,
-  });
-  return res.json(newBook);
+router.get("/books/:id" , async (req, res) => {
+  
+})
+
+router.post("/books", async (req, res) => {
+  const book = await prisma.book.findFirst({where:{title: req.body.title}});
+  if(!book) {
+    const newBook = await prisma.book.create({
+      data: req.body,
+    });
+    return res.json(newBook);
+  }
+  return res.json("El libro ya existe");
+});
+
+router.put("/books/:id", async (req, res) => {
 
 })
+
+router.delete("/books/:id", async (req, res) => {
+  
+})
+
 export default router;
