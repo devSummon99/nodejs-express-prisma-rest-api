d76f9990f8928925a23ade4bea747b94b7ca3028
devSummon99 <michelperezsomoza@gmail.com>
2024-09-01 21:38:36 -0400

    rest api first part finished

--- src/routes/authors.routes.js
+++ src/routes/authors.routes.js
@@ -1,20 +1,27 @@
 import { Router } from "express";
 
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "../db.js";
 
 const router = Router();
-const prisma = new PrismaClient();
 
 router.get("/authors", async (req, res) => {
   const authors = await prisma.author.findMany();
 
   return authors.length > 0
     ? res.json(authors)
-    : res.json("No existe ningun libro");
+    : res.json("No existe ningun autor");
 });
 
 router.get("/authors/:id" , async (req, res) => {
-  
+  const authorFound = await prisma.author.findFirst(
+    {where: {
+        id: parseInt(req.params.id)
+      }
+    })
+    if(authorFound) {
+      return res.json(authorFound);
+    }
+    return res.json("El autor buscado no existe");
 })
 
 router.post("/authors", async (req, res) => {
@@ -29,11 +36,39 @@ router.post("/authors", async (req, res) => {
 });
 
 router.put("/authors/:id", async (req, res) => {
-
+  const authorFound = await prisma.author.findFirst({
+    where: {
+      id: parseInt(req.params.id),
+    },
+  });
+  if (authorFound) {
+    await prisma.author.update({
+      where: {
+        id: parseInt(req.params.id)
+      },
+      data: req.body,
+    });
+    return res.status(204).json("El autor se ha modificado con éxito");
+  }
+  return res.status(404).json("El autor buscado no existe");
 })
 
 router.delete("/authors/:id", async (req, res) => {
-
-})
+  const authorFound = await prisma.author.findFirst({
+    where: {
+      id: parseInt(req.params.id),
+    },
+  });
+  if (authorFound) {
+    const authorDelete = await prisma.author.delete({
+      where: {
+        id: parseInt(req.params.id),
+      }})
+      if(authorDelete) {
+        return res.status(203).json("El autor se ha eliminado correctamente")
+      }
+    }
+    return res.status(404).json("El autor buscado no existe");
+  })
 
 export default router;
--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -1,27 +1,38 @@
 import { Router } from "express";
 
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "../db.js";
 
 const router = Router();
-const prisma = new PrismaClient();
 
 router.get("/books", async (req, res) => {
-  const books = await prisma.book.findMany(
-    {include: {author:true}},
-  );
+  const books = await prisma.book.findMany({
+    include: {
+      author: true,
+    },
+  });
 
   return books.length > 0
     ? res.json(books)
     : res.json("No se encontraron libros");
 });
 
-router.get("/books/:id" , async (req, res) => {
-  
-})
+router.get("/books/:id", async (req, res) => {
+  const bookFound = await prisma.book.findFirst({
+    where: {
+      id: parseInt(req.params.id),
+    },
+  });
+  if (bookFound) {
+    return res.json(bookFound);
+  }
+  return res.status(404).json("El libro buscado no existe");
+});
 
 router.post("/books", async (req, res) => {
-  const book = await prisma.book.findFirst({where:{title: req.body.title}});
-  if(!book) {
+  const book = await prisma.book.findFirst({
+    where: { title: req.body.title },
+  });
+  if (!book) {
     const newBook = await prisma.book.create({
       data: req.body,
     });
@@ -31,11 +42,39 @@ router.post("/books", async (req, res) => {
 });
 
 router.put("/books/:id", async (req, res) => {
-
-})
+  const bookFound = await prisma.book.findFirst({
+    where: {
+      id: parseInt(req.params.id),
+    },
+  });
+  if (bookFound) {
+    await prisma.book.update({
+      where: {
+        id: parseInt(req.params.id),
+      },
+      data: req.body,
+    });
+    return res.status(204).json("El libro se ha modificado con éxito");
+  }
+  return res.status(404).json("El libro buscado no existe");
+});
 
 router.delete("/books/:id", async (req, res) => {
-  
-})
+  const bookFound = await prisma.book.findFirst({
+    where: {
+      id: parseInt(req.params.id),
+    },
+  });
+  if (bookFound) {
+    const bookDelete = await prisma.book.delete({
+      where: {
+        id: parseInt(req.params.id),
+      }})
+      if(bookDelete) {
+        return res.status(203).json("El libro se ha eliminado correctamente")
+      }
+    }
+    return res.status(404).json("El libro buscado no existe");
+});
 
 export default router;
