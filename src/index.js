82881f2cea9755cd760878474d067e8461cdbf9a
devSummon99 <michelperezsomoza@gmail.com>
2024-09-08 02:05:32 -0400

    poco a poco

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-import express from "express";
-
-import authorsRoutes from "./routes/authors.routes.js";
-import bookssRoutes from "./routes/books.routes.js";
-
-const app = express();
-
-app.use(express.json());
-
-app.use("/api-books", authorsRoutes);
-
-app.use("/api-books", bookssRoutes);
-
+import { app } from "./app.js";
 app.listen(3000);
-console.log('server on port',3000);
+console.log("server on port", 3000);
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+import { prisma } from "../db.js";
+
+const router = Router();
+
+router.post("/auth/login", async (req,res)  => {
+    await  "Hello"
+})
+
+router.post("/auth/register", async (req,res)  => {
+    await  "Hello"
+})
