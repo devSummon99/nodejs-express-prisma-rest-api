a6f875829319898032eac91207d7588f918677a1
devSummon99 <michelperezsomoza@gmail.com>
2024-09-12 00:55:08 -0400

    starting authentication

--- src/controllers/authors.controller.js
+++ src/controllers/authors.controller.js
@@ -1,24 +1,68 @@
 import { prisma } from "../db.js";
 
 export const getAuthors = async (req, res) => {
-  res.send("Obteniendo autores");
+  const authors = await prisma.author.findMany();
+
+  return authors.length > 0
+    ? res.json(authors)
+    : res.json("No existe ningun autor");
 };
 
 export const getAuthorByID = async (req, res) => {
   const id = parseInt(req.params.id);
-  res.send("Buscando autores por id");
+  const authorFound = await prisma.author.findFirst({
+    where: {
+      id: id,
+    },
+  });
+
+  return authorFound
+    ? res.json(authorFound)
+    : res.json("El autor buscado no existe");
 };
 
 export const createAuthor = async (req, res) => {
-  res.send("Creando autor");
+  const data = req.body;
+  const author = await prisma.author.findFirst({ where: { name: data.name } });
+
+  return author
+    ? res.json("El autor ya existe")
+    : (await prisma.author.create({
+        data: req.body,
+      })) && res.json(req.body);
 };
 
 export const updateAuthorByID = async (req, res) => {
   const id = parseInt(req.params.id);
-  res.send("Modificando autor por id");
+  const data = req.body;
+  const authorFound = await prisma.author.findFirst({
+    where: {
+      id: id,
+    },
+  });
+
+  return authorFound
+    ? (await prisma.author.update({
+        where: {
+          id: id,
+        },
+        data: data,
+      })) && res.status(203).json("El autor se ha modificado con éxito")
+    : res.status(404).json("El autor buscado no existe");
 };
 
 export const deleteAuthorByID = async (req, res) => {
   const id = parseInt(req.params.id);
-  res.send("El autor se ha eliminado correctamente");
+  const authorFound = await prisma.author.findFirst({
+    where: {
+      id: id,
+    },
+  });
+  return authorFound
+    ? (await prisma.author.delete({
+        where: {
+          id: id,
+        },
+      })) && res.status(203).json("El autor se ha eliminado correctamente")
+    : res.status(404).json("El autor buscado no existe");
 };
--- src/controllers/books.controller.js
+++ src/controllers/books.controller.js
@@ -1,24 +1,68 @@
 import { prisma } from "../db.js";
 
 export const getBooks = async (req, res) => {
-  res.send("Obteniendo libros");
+  const books = await prisma.book.findMany();
+
+  return books.length > 0
+    ? res.json(books)
+    : res.json("No existe ningun libro");
 };
 
 export const getBookByID = async (req, res) => {
   const id = parseInt(req.params.id);
-  res.send("Buscando libros por id");
+  const bookFound = await prisma.book.findFirst({
+    where: {
+      id: id,
+    },
+  });
+
+  return bookFound
+    ? res.json(bookFound)
+    : res.json("El libro buscado no existe");
 };
 
 export const createBook = async (req, res) => {
-  res.send("Creando libro");
+  const data = req.body;
+  const book = await prisma.book.findFirst({ where: { title: data.title } });
+
+  return book
+    ? res.json("El libro ya existe")
+    : (await prisma.book.create({
+        data: req.body,
+      })) && res.json(req.body);
 };
 
 export const updateBookByID = async (req, res) => {
   const id = parseInt(req.params.id);
-  res.send("Modificando libro por id");
+  const data = req.body;
+  const bookFound = await prisma.book.findFirst({
+    where: {
+      id: id,
+    },
+  });
+
+  return bookFound
+    ? (await prisma.book.update({
+        where: {
+          id: id,
+        },
+        data: data,
+      })) && res.status(203).json("El libro se ha modificado con éxito")
+    : res.status(404).json("El libro buscado no existe");
 };
 
 export const deleteBookByID = async (req, res) => {
   const id = parseInt(req.params.id);
-  res.send("El libro se ha eliminado correctamente");
+  const bookFound = await prisma.book.findFirst({
+    where: {
+      id: id,
+    },
+  });
+  return bookFound
+    ? (await prisma.book.delete({
+        where: {
+          id: id,
+        },
+      })) && res.status(203).json("El libro se ha eliminado correctamente")
+    : res.status(404).json("El libro buscado no existe");
 };
