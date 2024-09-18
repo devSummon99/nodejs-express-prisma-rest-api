70c0ef1b9c28145829edb7485dcf57f1fbb13a90
devSummon99 <michelperezsomoza@gmail.com>
2024-09-18 04:58:48 -0400

    register

--- src/auth/controllers/auth.controller.js
+++ src/auth/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import { prisma } from "../../db.js";
+import jwt from "jsonwebtoken";
 
 export const Login = async (req,res)  => {
     await "Hello"
@@ -6,5 +7,17 @@ export const Login = async (req,res)  => {
 }
 
 export const Register = async (req,res)  => {
-    await  "Hello"
+    const { username, password, email } = req.body;
+  const user = await prisma.user.findFirst({ where: { email: email } });
+  const token = jwt.sign({ id: user.id }, username, password);
+
+  return user
+    ? res.json("El usuario ya existe")
+    : (await prisma.user.create({
+        data: {
+          username,
+          email,
+          password: hashSync(password, 10),
+        },
+      })) && res.json("El usuario fue creado correctamente");
 }
