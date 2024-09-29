3a4c4e174bbdc61dd3d6e0101876bbbace3f53e1
devSummon99 <michelperezsomoza@gmail.com>
2024-09-29 19:01:16 -0400

    users-test-done

--- src/users/tests/user.spec.js
+++ src/users/tests/user.spec.js
@@ -1,52 +1,102 @@
-import app from "../../app.js"
-/*
+import app from "../../app.js";
+import request from "supertest";
+
+import generarPalabrasAleatorias from "../../utils/words.js";
+
 describe("GET /api-books/users", () => {
+
     test("should response status code 200", async () => {
-        const response = await request(app).post("/api-books/auth/login").send({
-            email: "pedroperez31@cuba.cu",
-            password: "72658466",
-        })
-        expect(response.statusCode).toBe(200);
+        const response = await request(app).get("/api-books/users/").send()
+        expect(response.statusCode).toBe(200) && expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
+    })
+
+    test.skip("should response status code 404 and don't exists any users", async () => {
+        const response = await request(app).get("/api-books/users/").send()
+        expect(response.statusCode).toBe(404) && expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
     })
 })
 
 describe("GET /api-books/users/:id", () => {
+
     test("should response status code 200", async () => {
-        const response = await request(app).post("/api-books/auth/login").send({
-            email: "pedroperez31@cuba.cu",
-            password: "72658466",
-        })
-        expect(response.statusCode).toBe(200);
+        const response = await request(app).get("/api-books/users/1").send()
+        expect(response.statusCode).toBe(200) && expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
+    })
+
+    test("should response status code 404 and don't exists this users", async () => {
+        const response = await request(app).get("/api-books/users/100").send()
+        expect(response.statusCode).toBe(404) && expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
     })
 })
 
 describe("POST /api-books/users", () => {
-    test("should response status code 200", async () => {
-        const response = await request(app).post("/api-books/auth/login").send({
-            email: "pedroperez31@cuba.cu",
-            password: "72658466",
+
+    test.skip("should response status code 201", async () => {
+        const response = await request(app).post("/api-books/users/").send({
+            username: `${generarPalabrasAleatorias(1)}`,
+            email: `${generarPalabrasAleatorias(1)}@gmail.com`,
+            password: "12345678"
         })
-        expect(response.statusCode).toBe(200);
+        expect(response.statusCode).toBe(201) && expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
+    })
+
+    test.skip("should response status code 400", async () => {
+        const response = await request(app).post("/api-books/users/").send({
+            username: `micho`,
+            email: "rm2333@cu.cu",
+            password: "12345678"
+        })
+        expect(response.statusCode).toBe(400) && expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
+    })
+
+    test("should response status code 401", async () => {
+        const response = await request(app).post("/api-books/users/").send({
+            username: `micho`,
+            email: "rm2333@cu.cu",
+            password: "12345678"
+        })
+        expect(response.statusCode).toBe(401) && expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
     })
 })
 
 describe("PUT /api-books/users/:id", () => {
-    test("should response status code 200", async () => {
-        const response = await request(app).post("/api-books/auth/login").send({
-            email: "pedroperez31@cuba.cu",
-            password: "72658466",
+
+    test.skip("should response status code 203", async () => {
+        const response = await request(app).put("/api-books/users/5").send({
+              password: "12345679"
         })
-        expect(response.statusCode).toBe(200);
+        expect(response.statusCode).toBe(203) && expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
+    })
+
+    test.skip("should response status code 404 and message don't exists this book", async () => {
+        const response = await request(app).put("/api-books/users/100").send({
+            password: "12345679"
+        })
+        expect(response.statusCode).toBe(404) && expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
+    })
+
+    test("should response status code 401", async () => {
+        const response = await request(app).put("/api-books/users/8").send({
+            password: "12345679"
+        })
+        expect(response.statusCode).toBe(401) && expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
     })
 })
 
 describe("DELETE /api-books/users/:id", () => {
-    test("should response status code 200", async () => {
-        
-        const response = await request(app).post("/api-books/auth/login").send({
-            email: "pedroperez31@cuba.cu",
-            password: "72658466",
-        })
-        expect(response.statusCode).toBe(200);
+
+    test.skip("should response status code 203", async () => {
+        const response = await request(app).delete("/api-books/users/8").send()
+        expect(response.statusCode).toBe(203) && expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
     })
-})*/
+
+    test.skip("should response status code 404 and message don't exists this author", async () => {
+        const response = await request(app).delete("/api-books/users/100").send()
+        expect(response.statusCode).toBe(404) && expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
+    })
+
+    test("should response status code 401", async () => {
+        const response = await request(app).delete("/api-books/users/8").send()
+        expect(response.statusCode).toBe(401) && expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
+    })
+})
